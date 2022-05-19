7e8aaaea87cba14347a50dff6de76fcef627897b
Alton Ong <alton_ong@live.com.sg>
2022-05-19 18:58:08 +1000

    Added global emissions carbon dioxide graph

--- public/index.js
+++ public/index.js
@@ -10,32 +10,45 @@ const countryTInput = document.getElementById("countriesT");
 
 const loadData = () => {
 	Plotly.d3.csv(emissionsData, (data) => {
-		processEmissionData(data, "Australia", "Indonesia");
+		processEmissionData(data, "Australia", "Philippines");
 	});
 };
 
 const processEmissionData = (allRows, country1, country2) => {
 	// Initialise empty arrays to store individual values
-	let years = [],
+	let yearsC1= [],
+		yearsC2= [],
+		yearsG = [],
 		co2ProdCountry1 = [],
-		co2ProdCountry2 = [];
-	// Initialise variables
+		co2ProdCountry2 = [],
+		co2ProdWorld = [];
 
+	// Initialise variables
+	const startingYear = 1900
 	for (let i = 0; i < allRows.length; i++) {
 		let row = allRows[i];
 
-		if (parseInt(row["year"]) > 2000 && row["country"] === country1) {
-			years.push(row["year"]);
-			co2ProdCountry1.push(row["co2_per_capita"]);
+		if (parseInt(row["year"]) >= startingYear && row["country"] === country1) {
+			yearsC1.push(row["year"]);
+			co2ProdCountry1.push(row["co2"]);
 		}
-		if (parseInt(row["year"]) > 2000 && row["country"] === country2) {
-			years.push(row["year"]);
-			co2ProdCountry2.push(row["co2_per_capita"]);
+		if (parseInt(row["year"]) >= startingYear && row["country"] === country2) {
+			yearsC2.push(row["year"])
+			co2ProdCountry2.push(row["co2"]);
+		}
+		if(parseInt(row["year"]) >= startingYear && row["country"] === "World"){
+			yearsG.push(row["year"])
+			co2ProdWorld.push(row["co2"]);
 		}
 	}
+
+
+	// console.log("Years:", years)
+	console.log(allRows.length)
 	console.log("Information processed");
-	makeEmissionsPlotF(
-		years,
+	makeEmissionsPlot(
+		yearsC1,
+		yearsC2,
 		co2ProdCountry1,
 		co2ProdCountry2,
 		country1,
@@ -43,27 +56,47 @@ const processEmissionData = (allRows, country1, country2) => {
 		"First World",
 		"carbonProdPlotF"
 	);
-	// makeEmissionsPlotF(
-	// 	years,
-	// 	co2ProdCountry2,
-	// 	country2,
-	// 	"Third World",
-	// 	"carbonProdPlotT"
-	// );
-};
+	makeWorldEmissionsPlot(yearsG, co2ProdWorld, "worldCarbonEmissionsGraph");
 
-const makeEmissionsPlotF = (years, co2Prod1, co2Prod2, country1, country2, type, plot) => {
+};
+const makeWorldEmissionsPlot = (years, co2Prod, plot) => {
+	let traces = [{
+		type: "scatter",
+		x: years,
+		y: co2Prod,
+		name: "Global carbon emissions",
+		mode: "lines+markers"
+	}]
+	let layout = {
+		title: "Global emissions of carbon dioxide",
+		font: {
+			color: "white"
+		},
+		axis: {
+			title: "Year"
+		},
+		yaxis: {
+			title: "Production of carbon dioxide in million tonnes"
+		},
+		paper_bgcolor: "rgba(0,0,0,0)",
+		plot_bgcolor: "rgba(0,0,0,0)",
+		width: 1500,
+		height: 800
+	}
+	Plotly.newPlot(plot, traces, layout);
+}
+const makeEmissionsPlot = (yearsC1, yearsC2, co2Prod1, co2Prod2, country1, country2, type, plot) => {
 	let traces = [
 		{
 			type: "scatter",
-			x: years,
+			x: yearsC1,
 			y: co2Prod1,
 			name: country1,
 			mode: "lines+markers"
 		},
 		{
 			type: "scatter",
-			x: years,
+			x: yearsC2,
 			y: co2Prod2,
 			name: country2,
 			mode: "lines+markers"
